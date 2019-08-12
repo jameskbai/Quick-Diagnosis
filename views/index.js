092e570a0cb00745993e0a0cb86816fbd118170c
James Bai <jamesbai@Jamess-MacBook-Pro-4.local>
2019-08-12 10:19:19 -0700

    Updated body parts list

--- views/index.js
+++ views/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
+  function askBodyPartsQuestions(data) {
+    // take data and give a multiple choice question.
+    for (var i = 0; i < data.length; i++) {
+      var bodypart = data[i];
+      $(".column1").append("<p>" + bodypart.Name + "</p>");
+    }
+  }
+
   var url = '/bodyparts';
   console.log($);
   $.ajax(url)
   .done(function(data) {
     console.log(data);
+    askBodyPartsQuestions(data);
   })
   .fail(function() {
     console.log('it failed');
