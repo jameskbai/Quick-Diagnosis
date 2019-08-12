165e104e5b31f517c2fbd8e631e1a00035a907a2
SoRa Bae <srbae1@berkeley.edu>
2019-08-12 12:57:27 -0700

    Add detailed body parts backend

--- api/api_medic.js
+++ api/api_medic.js
@@ -1,5 +1,6 @@
 var token = require('../config/token').token;
 var body_location_url = "https://sandbox-healthservice.priaid.ch/body/locations?token=" + token + "&format=json&language=en-gb";
+var detailed_body_url = "https://sandbox-healthservice.priaid.ch/body/locations/%s?token=" + token + "&format=json&language=en-gb"
 var body_symptom_url = "https://sandbox-healthservice.priaid.ch/symptoms/%s/%s?token=" + token + "&format=json&language=en-gb";
 var diagnosis_url = "https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=%s&gender=%s&year_of_birth=%s&token=" + token + "&format=json&language=en-gb";
 var specialization_url = "https://sandbox-healthservice.priaid.ch/diagnosis/specialisations?symptoms=%s&gender=%s&year_of_birth=%s&token=" + token + "&format=json&language=en-gb";
@@ -11,6 +12,13 @@ module.exports = {
       request(body_location_url, function (err, apiResp) {
           res.json(JSON.parse(apiResp.body));
       });
+  },
+
+  getDetailedBodyParts: function(req, res) {
+    var id = req.params.id;
+    request(util.format(detailed_body_url, id), function(err, apiResp) {
+      res.json(JSON.parse(apiResp.body));
+    })
   },
   getSymptomsForBodyPart: function (req, res) {
       var gender = req.params.gender || 0;
--- index.js
+++ index.js
@@ -11,7 +11,8 @@ process.on('uncaughtException', function (err) {
 app.use(express.static(__dirname + '/views'));
 
 // routes for the app
-app.get('/bodyparts', api_medic.getBodyParts); 
+app.get('/bodyparts', api_medic.getBodyParts);
+app.get('/detailedBodypart/:id', api_medic.getDetailedBodyParts);
 app.get('/symptoms/:bodyPart/:gender', api_medic.getSymptomsForBodyPart);
 app.get('/diagnosis', api_medic.getDiagnosis);
 app.get('/specialization', api_medic.getSpecialization);
@@ -19,4 +20,3 @@ app.get('/specialization', api_medic.getSpecialization);
 var server = http.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
-
