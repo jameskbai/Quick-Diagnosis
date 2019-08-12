6f2bc0ce3ed705093685e91d2134917bc26893ff
James Bai <jamesbai@Jamess-MacBook-Pro-4.local>
2019-08-12 16:08:28 -0700

    dropdown content

--- views/index.js
+++ views/index.js
@@ -4,19 +4,21 @@ $(document).ready(function() {
     // take data and give a multiple choice question.
     for (var i = 0; i < data.length; i++) {
       var bodyPart = data[i];
-      var bodyName = "<button  id =" + bodyPart.ID + " class=dropbtn>" + bodyPart.Name + "</button>";
+      console.log("names output1: " + bodyPart.Name);
+      var bodyName = "<p id =p_" + bodyPart.ID + "><button  id =" + bodyPart.ID + " class=dropbtn>" + bodyPart.Name + "</button></p";
       $(".bodyButton").append(bodyName);
-      
-
-
     }
 
-  // function showDetailedBodyParts(data, curr) {
-  //   for (var i = 0; i < data.length; i++) 
-  //     var detailedBodyPart = data[i];
-  //     var detailedBodyName = "<button class=dropdown-content>" + detailedBodyPart.Name + "</button>";
-  //     $(curr).append(detailedBodyName);
-  // }
+  function showDetailedBodyParts(data, curr) {
+    for (var i = 0; i < data.length; i++) {
+      var detailedBodyPart = data[i];
+      console.log("names output2:" + detailedBodyPart.Name);
+      var detailedBodyName = "<p><button id =" + detailedBodyPart.ID + " class=dropdown-content>" + detailedBodyPart.Name + "</button></p>";
+      var idName = "#p_" + curr.id;
+
+      $(idName).append(detailedBodyName);
+     } 
+  }
 
     $(".dropbtn").click(function(){
       console.log("id here: " + this.id);
@@ -25,14 +27,18 @@ $(document).ready(function() {
       $.ajax(url2)
       .done(function(data) {
         console.log(data);
-        for (var i = 0; i < data.length; i++) 
-          var detailedBodyPart = data[i];
-          var detailedBodyName = "<button class=dropdown-content>" + detailedBodyPart.Name + "</button>";
-          curr.append(detailedBodyName);
+        showDetailedBodyParts(data, curr);
       })
       .fail(function() {
         console.log('it failed');
       })
+    })
+
+    var bodyPartIds;
+    $(".dropdown-content").click(function(){
+      console.log("hello");
+      bodyPartIds.push(this.id);
+      console.log(bodyPartIds);
     })
 
 
